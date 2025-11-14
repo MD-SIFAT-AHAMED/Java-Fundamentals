baf52c99d433c15480731b2c393f98db8169dd37
MD SIFAT AHAMED <mdsifat978@gmail.com>
2025-11-14 23:25:46 +0600

    dataType

---
+++ Day1/dataTypes.java
@@ -0,0 +1,16 @@
+public class dataTypes {
+    public static void main(String arg[]) {
+        int num1 = 9;
+        byte by = 125; // byte 128 to 127 store
+        short sh = 599;
+        long l = 78495l;
+
+        float f = 5.5f;
+        double d = 5.8;
+
+        char c = 'a';
+
+        boolean b = true;
+
+    }
+}
